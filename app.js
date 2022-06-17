1be4d9e5cf542ac3c1dbd074173852fafd68b231
Aochen <aochen@pku.edu.cn>
2022-06-17 14:53:34 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -3,8 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-var lodash = require('lodash');
-
+const _ = require("lodash");
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/journalDB');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -21,37 +22,32 @@ app.use(express.static("public"));
 
 let posts = [];
 
-app.get("/", function(req, res) {
-    res.render("home", {
-        homeContent: homeStartingContent,
-        posts: posts
-    });
+const postSchema = new mongoose.Schema({
+    title: String,
+    content: String
 });
+const Post = mongoose.model('Post', postSchema);
 
-app.get('/posts/:item', function(req, res) {
-    let reqTitle = lodash.lowerCase(req.params.item);
 
-    posts.forEach(function(post) {
-        let storedTitle = lodash.lowerCase(post.title);
-        if (storedTitle === reqTitle) {
-            res.render("post", {
-                postTitle: post.title,
-                postBody: post.body
-            });
-        };
+app.get("/", function(req, res) {
+    Post.find(function(err, posts) {
+        res.render("home", {
+            startingContent: homeStartingContent,
+            posts: posts
+        });
     });
 
 });
 
 app.get("/about", function(req, res) {
     res.render("about", {
-        aboutContent: aboutContent,
+        aboutContent: aboutContent
     });
 });
 
 app.get("/contact", function(req, res) {
     res.render("contact", {
-        contactContent: contactContent,
+        contactContent: contactContent
     });
 });
 
@@ -59,15 +55,37 @@ app.get("/compose", function(req, res) {
     res.render("compose");
 });
 
-app.post("/", function(req, res) {
-    const post = {
-        title: req.body.postTitle,
-        body: req.body.postBody
-    };
-    posts.push(post);
-    res.redirect("/");
+app.post("/compose", function(req, res) {
+
+    const post = new Post({
+        title: _.capitalize(req.body.postTitle),
+        content: req.body.postBody
+    });
+
+    post.save(function(err) {
+        if (!err) {
+            res.redirect("/");
+        }
+    });
+
+
+
 });
 
+app.get("/posts/:postId", function(req, res) {
+    const requestedId = req.params.postId;
+    console.log(requestedId);
+
+    Post.findById(requestedId, function(err, foundPost) {
+        if (!err) {
+            res.render("post", {
+                title: foundPost.title,
+                content: foundPost.content
+            });
+        }
+    });
+
+});
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
